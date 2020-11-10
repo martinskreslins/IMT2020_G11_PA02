1df3399ecf76d725c4963316a8c93d9dfa2fa4ce
Martins Kreslins <martins.kreslins@va.lv>
2020-11-10 14:52:18 +0200

    fixes some errors

--- App.js
+++ App.js
@@ -5,11 +5,11 @@ import { StyleSheet, Text, View, Image} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>
-        <p>Hello World from Group 11 Member: Martins Kreslins!</p>
-        <p>This is my Martins Kreslins first React Native application!<br/>
-        No idea why it works. But it works!</p>
-        </Text>
+      
+      <Text> Hello World from Group 11 Member: Martins Kreslins!</Text>
+      <Text>This is my Martins Kreslins first React Native application!</Text>
+      <Text> No idea why it works. But it works!</Text>
+
         <Image
           source= {{uri: "https://pbs.twimg.com/profile_images/1113929952406061056/QZdZjFUw_400x400.jpg"}}
           style= {{width:200, height:200}}
