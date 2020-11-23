a8287ef7a0dc5378abcb3e658c2c10e4b632ed6a
Denis Chernov <67118409+zoyazip@users.noreply.github.com>
2020-11-23 23:55:45 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
 
 export default function App() {
+  var name = 'Denis Chernov';
+  name.style = {color: '#939FC5'}
   return (
     <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello World from Group G11 <br/>
+        Member: {name}<br/>       
+        <p>This is my {name}<br/>
+         first React Native application!<br/>
+         Nice to see you here!</p>
+      </Text>
+      <image
+        source = {require('./assets/snack-icon.png')}
+      />
       
-      <Text> Hello World from Group 11 Member: Martins Kreslins!</Text>
-      <Text>This is my Martins Kreslins first React Native application!</Text>
-        <Image
-          source= {require('./peepoHappi.jpg')}
-          style= {{width:200, height:200}}
-        />
-        <Text> I am happy that this works!</Text>
+      <Card>
+        <AssetExample />
+      </Card>
     </View>
   );
 }
@@ -20,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 14,
+    textAlign: 'left',
+    color: '#9AA4B3'
   },
 });
